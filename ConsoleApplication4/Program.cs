1b69149f586d24e59cbaf27cc84180ba0a785041
Your Name <you@example.com>
2015-09-27 19:01:04 -0400

    4

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+                Console.WriteLine("Enter Age");
+            
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+           
+
+            if (x >= 17)
+            {
+                Console.WriteLine("You can enter.");
+            }
+            
+              else 
+
+               {
+                    Console.WriteLine("Do not enter!");
+                }
+
+            
+        }
+    }
+}
